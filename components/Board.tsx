f8e5685bea185876bbc92640865413576cdd5c12
Chris Birster <christopher.birster@gmail.com>
2023-02-08 14:12:42 -0500

    add logic for win/draw/filled square

--- components/Board.tsx
+++ components/Board.tsx
@@ -8,18 +8,64 @@ export default function Board() {
 
     const handlePlayer = () => xIsNext ? 'X' : 'O';
 
+    const checkWinner = (squares: Array<string>) => {
+        const winningLines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+
+        for (let i = 0; i < winningLines.length; i++) {
+            const [a, b, c] = winningLines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
+    }
+
+    const checkDraw = (squares: Array<string>) => {
+        for (let i = 0; i < squares.length; i++) {
+            if (squares[i] === '') {
+                return false;
+            }
+        }
+        return true;
+    }
+
     const handlePress = (i: number) => {
+        if (squares[i]) {
+            return;
+        }
 
         // create a copy of the squares array
         const squaresCopy = squares.slice();
 
         // set the square to the current player
         squaresCopy[i] = handlePlayer();
+
         // set the squares state to the copy
         setSquares(squaresCopy);
 
         // set the next player
         setXIsNext(!xIsNext);
+
+        const winnerMaybe = checkWinner(squaresCopy);
+        if (winnerMaybe) {
+            console.log(`Winner is ${winnerMaybe}`);
+            return;
+        }
+
+        if (checkDraw(squaresCopy)) {
+            console.log('Draw');
+            return;
+        }
+
     }
 
     return (
