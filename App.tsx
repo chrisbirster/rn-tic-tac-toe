b1fa0fd61cec3537d6ae7442cd0303888135cbd7
Chris Birster <christopher.birster@gmail.com>
2023-02-08 10:34:18 -0500

    setup a static board with rows and squares

--- App.tsx
+++ App.tsx
@@ -1,20 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import { Pressable, StyleSheet, Text, View } from 'react-native';
 
-export function Square() {
+export function Square({text}: {text: string}) {
   return (
     <Pressable style={styles.square}>
       <View>
-        <Text style={styles.squareText}>X</Text>
+        <Text style={styles.squareText}>{text}</Text>
       </View>
     </Pressable>
+  )
+}
+export function Row({ children }: { children: React.ReactNode }) {
+  return (
+    <View style={styles.row}>
+      {children}
+    </View>
   )
 }
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Square />
+      <Row>
+        <Square text='1'/>
+        <Square text='2'/>
+        <Square text='3'/>
+      </Row>
+      <Row>
+        <Square text='4'/>
+        <Square text='5'/>
+        <Square text='6'/>
+      </Row>
+      <Row>
+        <Square text='7'/>
+        <Square text='8'/>
+        <Square text='9'/>
+      </Row>
       <StatusBar style="auto" />
     </View>
   );
@@ -28,12 +49,17 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   square: {
+    justifyContent: 'center',
     borderWidth: 1,
-    paddingHorizontal: 15,
+    width: 80,
+    height: 80,
   },
   squareText: {
     fontSize: 60,
     fontWeight: 'bold',
     textAlign: 'center',
   },
+  row: {
+    flexDirection: 'row',
+  }
 });
